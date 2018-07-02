a51d19c9f49a76e44c76bb819234f3390b9359fe
Andrea <afemerick@gmail.com>
2018-07-01 21:48:31 -0400

    Initial commit

---
+++ src/com/company/NumberGuesser.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/*
+ * An interface for the number guesser java object.
+ * This class will handle user input parsing and validation,
+ * number guessing logic and "try again" loop control.
+ */
+public interface NumberGuesser {
+}
